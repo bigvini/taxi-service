4117dd7891d124528c084c57b88947a0a57b40ce
Vershynin Kostia <bigvini@gmail.com>
2023-08-23 23:13:35 +0300

    add service taxi

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL
-            = "jdbc:mysql://localhost:3306/taxi?serverTimezone=Europe/Kiev";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1984";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "URL";
+    private static final String USERNAME = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String JDBC_DRIVER = "JDBC DRIVER";
 
     static {
         try {
